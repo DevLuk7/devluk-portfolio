51376e8c7eb19339b43e86b6d9fb2d7feb054ce0
Lukasz Kumiec <505075181@penske.com>
2025-06-03 14:07:28 +0200

    Update lucide icon

--- src/app/home/section-side-projects/section-side-projects.component.ts
+++ src/app/home/section-side-projects/section-side-projects.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ExternalLink, LucideAngularModule } from 'lucide-angular';
 import { SkillBadgeComponent } from '../../ui/skill-badge/skill-badge.component';
 import {
   Project,
@@ -10,11 +9,10 @@ import {
 @Component({
   selector: 'app-section-side-projects',
   standalone: true,
-  imports: [CommonModule, SkillBadgeComponent, LucideAngularModule],
+  imports: [CommonModule, SkillBadgeComponent],
   templateUrl: './section-side-projects.component.html',
   styleUrl: './section-side-projects.component.scss',
 })
 export class SectionSideProjectsComponent {
   readonly workProjects: Project[] = inject(ProjectsService).sideProjects;
-  readonly externalLinkIcon = ExternalLink;
 }
