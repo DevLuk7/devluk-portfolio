8593ba3a605cdc877c5bba5a538cc0ae3a2e85c4
Lukasz Kumiec <505075181@penske.com>
2025-06-05 22:09:10 +0200

    Add angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,8 @@ import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { WINDOW } from './utils/window';
 import { isPlatformBrowser } from '@angular/common';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai';
 
 export const config = () => {
   if (isDevMode()) {
@@ -36,5 +38,17 @@ export const appConfig: ApplicationConfig = {
       },
       deps: [PLATFORM_ID],
     },
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'portfolio-83e54',
+        appId: '1:790412593531:web:eb9bc9efef9399b91895de',
+        storageBucket: 'portfolio-83e54.firebasestorage.app',
+        apiKey: 'AIzaSyCZ8b84maDDdGKgkVLl-yKpqbOzITyBECM',
+        authDomain: 'portfolio-83e54.firebaseapp.com',
+        messagingSenderId: '790412593531',
+        measurementId: 'G-DMM701QRZK',
+      })
+    ),
+    provideVertexAI(() => getVertexAI()),
   ],
 };
